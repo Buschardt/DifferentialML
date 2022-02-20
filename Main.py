e96b9db20f668908a4eb08fb103368fdbe067354
Buschardt <sebastianbuschardt@hotmail.com>
2022-02-20 12:29:38 +0100

    AAD implementation in MonteCarlo

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import NeuralNetwork.NN as nn
 import OptionType.EuroCall as ec
 import Models.BlackScholesModel as bsm
 import MonteCarlo.TimeDiscretization as td
-
 import MonteCarlo.BrownianMotion as bm
 import MonteCarlo.EulerSchemeFromProcessModel as ep
+from Products.EuropeanOption import Option
 
 import torch
 import matplotlib.pyplot as plt
@@ -18,8 +18,12 @@ def C_BS(S0, K, T, sigma, r):
 
     C = norm.cdf(d1)*S0 - norm.cdf(d2)*K*np.exp(-r*T)
     return C
+
+def deltaBS(S0, K, T, sigma, r):
+    d1 = 1/(sigma*np.sqrt(T)) * (np.log(S0/K) + (r+(sigma**2)/2) * T )
+    return norm.cdf(d1)
 #%%
-nSamples = 50000
+nSamples = 10000
 dt = 1
 sigma = 0.2
 K = 1.1
@@ -30,38 +34,53 @@ r = 0.03
 S0 = K + d * np.random.normal(0, 1, nSamples)
 S0 = np.linspace(0.01,3.5,nSamples)
 ST = np.empty(S0.shape[0])
+delta = np.empty(S0.shape[0])
 
+product = Option(1)
 time = td.TimeDiscretization(0, 1, 1)
 driver = bm.BrownianMotion(time, nSamples, 1, 0)
 driver.generateBM()
 model = bsm.BlackScholesModel(0.2, 0.03)
-process = ep.EulerSchemeFromProcessModel(model,driver,time, None)
+process = ep.EulerSchemeFromProcessModel(model,driver,time, product)
 for i in range(0,S0.shape[0]):
-    ST[i] = process.calculateProcess(S0[i], i)
+    S0_tensor = torch.tensor(S0[i])
+    ST[i] = process.calculateProcess(S0_tensor, i)
+    delta[i] = process.calculateDerivs(S0_tensor, i)
 
-#remove nan
-#S0 = S0[~np.isnan(S0)]
-#ST = ST[~np.isnan(ST)]
 
 Call = ec.EuroCall(ST, K, dt, r)
 C = Call.payoff()
 
 #Define and train net
-net = nn.NeuralNet(1,1,6,20)
-net.generateData(S0, C)
-net.train()
+net = nn.NeuralNet(1,1,6,20, differential=True)
+net.generateData(S0, C, delta)
+net.train(alpha=0.2, beta=0.8)
 
 #predict
 S0_test = np.linspace(0, 3.5, 100)
-y_test = net.predict(S0_test)
+y_test, dydx_test = net.predict(S0_test, True)
 
 #compare with true Black-Scholes price
 truePrice = C_BS(S0_test, K, 1, sigma, r)
+trueDelta = deltaBS(S0_test, K, 1, sigma, r)
 
+plt.figure(figsize=[14,8])
+plt.subplot(1, 2, 1)
 plt.plot(S0, C, 'o', color='grey', label='Simulated payoffs', alpha = 0.3)
 plt.plot(S0_test, y_test, color='red', label='NN approximation')
 plt.plot(S0_test, truePrice, color='black', label='Black-Scholes price')
+plt.xlabel('S0')
+plt.ylabel('C')
 plt.legend()
-plt.title('Standard ML - price approximation')
+plt.title('Differential ML - Price approximation')
+
+plt.subplot(1, 2, 2)
+plt.plot(S0, delta, 'o', color='grey', label='AAD deltas', alpha = 0.3)
+plt.plot(S0_test, dydx_test, color='red', label='NN approximated delta')
+plt.plot(S0_test, trueDelta, color='black', label='Black-Scholes delta')
+plt.xlabel('S0')
+plt.ylabel('delta')
+plt.legend()
+plt.title('Differential ML - Delta approximation')
 plt.show()
-#plt.savefig('NNtest.png')
+#plt.savefig('NNTest.png')
--- MonteCarlo/EulerSchemeFromProcessModel.py
+++ MonteCarlo/EulerSchemeFromProcessModel.py
@@ -15,18 +15,31 @@ class EulerSchemeFromProcessModel:
         self.stochasticDriver = stochasticDriver
         self.timeDiscretization = timeDiscretization
         self.discreteProcess = torch.empty((self.timeDiscretization.numberOfSteps+1))
-        
+        self.product = product
+
     def calculateProcess(self,initialState,pathNumber):
-        self.initialState = torch.tensor(initialState).requires_grad_()
-        self.discreteProcess[0] = torch.log(self.initialState)
+        self.initialState = initialState
+        #self.discreteProcess[0] = torch.log(self.initialState)
         if self.model.stationary:
             drift = self.model.getDrift(0,0)
             factorLoadings = self.model.getFactorLoadings(0,0)
             increments = self.stochasticDriver.getIncrement(pathNumber)
-            return torch.exp(torch.add(self.discreteProcess[0],torch.cumsum(torch.add(drift*self.timeDiscretization.deltaT,torch.mul(factorLoadings,increments)),dim=0)))
+            return torch.exp(torch.log(self.initialState) + torch.cumsum(drift * self.timeDiscretization.deltaT + factorLoadings * increments, axis=0))
+            #return torch.exp(torch.add(self.discreteProcess[0],torch.cumsum(torch.add(drift*self.timeDiscretization.deltaT,torch.mul(factorLoadings,increments)),dim=0)))
             #self.discreteProcess[1:] = torch.add(self.initialState,torch.cumsum(torch.add(drift*self.timeDiscretization.deltaT,torch.mul(factorLoadings,increments)),dim=0))    
             #return torch.exp(self.discreteProcess)
         else:
             raise NotImplementedError()
+
+    def pathPayoff(self, initialState, pathNumber):
+        S = self.calculateProcess(initialState, pathNumber)
+        V = self.product.payoff(S)
+
+        return V
+    
+    def calculateDerivs(self, initialState, pathNumber):
+        initialState = torch.tensor(initialState).requires_grad_()
+        f = self.pathPayoff(initialState, pathNumber)[-1]
+        grad = torch.autograd.grad(f, [initialState], allow_unused=True)
             
-        
+        return grad[0]
