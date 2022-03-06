5567bea79d986caed9d2f82d58a12a96500d2d39
Buschardt <sebastianbuschardt@hotmail.com>
2022-03-06 14:41:34 +0100

    Added Longstaff-Schwartz

--- Main.py
+++ Main.py
@@ -5,12 +5,13 @@ import MonteCarlo.TimeDiscretization as td
 import MonteCarlo.BrownianMotion as bm
 import MonteCarlo.EulerSchemeFromProcessModel as ep
 from Products.EuropeanOption import Option
+import Models.LongstaffSchwartz as ls
 
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
 #%%
-nSamples = 25000
+nSamples = 12500
 dt = 10
 T = 1.
 sigma = 0.2 + np.random.normal(0, 0.025, nSamples)
@@ -27,7 +28,7 @@ C = np.empty(S0.shape[0])
 X = np.c_[S0, sigma]
 greeks = np.empty((S0.shape[0], 2))
 
-product = Option(K, 'put')
+product = Option(K, 'call')
 time = td.TimeDiscretization(0, dt, T/dt)
 driver = bm.BrownianMotion(time, nSamples, 1)
 driver.generateBM()
@@ -54,9 +55,9 @@ X_test = np.c_[S0_test, sigma_test]
 y_test, dydx_test = net.predict(X_test, True)
 
 #compare with true Black-Scholes price
-truePrice = bsm.P(S0_test, K, T, sigma_test, r)
-trueDelta = bsm.delta(S0_test, K, T, sigma_test, r, 'Put')
-trueVega = bsm.vega(S0_test, K, T, sigma_test, r, 'Put')
+truePrice = bsm.C(S0_test, K, T, sigma_test, r)
+trueDelta = bsm.delta(S0_test, K, T, sigma_test, r, 'Call')
+trueVega = bsm.vega(S0_test, K, T, sigma_test, r, 'Call')
 
 plt.figure(figsize=[14,8])
 plt.subplot(1, 3, 1)
@@ -86,4 +87,64 @@ plt.ylabel('vega')
 plt.legend()
 plt.title('Differential ML - Vega approximation')
 plt.show()
-#plt.savefig('DiffNNTest3.png')
+#plt.savefig('./Plots/DiffNNTest3.png')
+
+#%%
+#____________________________________________________________________________
+###   Longstaff - Schwartz example   ###
+
+nSamples_LSM = 1000
+S_LSM = K + d * torch.normal(0, 1, size=(nSamples_LSM, 1))
+K_LSM = torch.tensor(1.)
+sigma_LSM = torch.tensor(0.2)
+r_LSM = torch.tensor(0.03)
+dts_LSM = torch.tensor([1., 1., 1., 1.]).view(1,4)
+
+C_LSM = torch.empty((S_LSM.shape[0]))
+greeks_LSM = torch.empty((S_LSM.shape[0]))
+St_LSM = torch.empty((S_LSM.shape[0], 4))
+for i in range(S_LSM.shape[0]):
+    if i % 100 == 0:
+        print(i, '/', nSamples_LSM)
+    Si = S_LSM[i]
+    dW = torch.randn(1,4)
+    St, Et = ls.genPaths(Si, K_LSM, sigma_LSM, r_LSM, dts_LSM, dW)
+    w, b = ls.LSM_train(St, Et)
+    Si.requires_grad_()
+    C_LSM[i] = ls.LSM(Si, K_LSM, sigma_LSM, r_LSM, dts_LSM, dW, w, b)
+    greeks_LSM[i] = torch.autograd.grad(C_LSM[i], [Si], allow_unused=True)[0]
+    
+print('Data generated')
+
+#Define and train net
+netLSM = nn.NeuralNet(1, 1, 6, 60, differential=True)
+netLSM.generateData(S_LSM.detach(), C_LSM.detach(), greeks_LSM)
+netLSM.prepare()
+netLSM.lambda_j = 1.0 / torch.sqrt((netLSM.dydx ** 2).mean(0)).reshape(1, -1)
+netLSM.train(n_epochs = 5, batch_size=10, alpha=0.1, beta=0.9, lr=0.001)
+
+#predict
+y_LSM_test, dydx_LSM_test = netLSM.predict(S0_test, gradients=True)
+
+plt.figure(figsize=[14,8])
+plt.subplot(1, 2, 1)
+plt.plot(S0, C, 'o', color='grey', alpha = 0.3, label='Euro samples')
+plt.plot(S_LSM.detach().numpy(), C_LSM.detach().numpy(), 'o', color='lightblue', alpha = 0.3, label='American samples')
+plt.plot(S0_test, y_test, color='red', label='NN - Euro')
+plt.plot(S0_test, y_LSM_test, color='navy', label='NN - American')
+plt.legend()
+plt.title(f'Euro (samples: {nSamples}) vs American option (samples: {nSamples_LSM})')
+plt.xlabel('S0')
+plt.ylabel('C')
+
+plt.subplot(1, 2, 2)
+plt.plot(S0, greeks[:,0], 'o', color='grey', alpha = 0.3, label='Euro delta')
+plt.plot(S_LSM.detach().numpy(), greeks_LSM.detach().numpy(), 'o', color='lightblue', alpha = 0.3, label='American delta')
+plt.plot(S0_test, dydx_test[:,0], color='red', label='NN - Euro')
+plt.plot(S0_test, dydx_LSM_test, color='navy', label='NN - American')
+plt.legend()
+plt.title(f'Euro (samples: {nSamples}) vs American option (samples: {nSamples_LSM})')
+plt.xlabel('S0')
+plt.ylabel('Delta')
+plt.show()
+#plt.savefig('./Plots/LongstaffvsEuro')
---
+++ Models/LongstaffSchwartz.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+
+#Linear model class
+class LinearModel(nn.Module):
+    def __init__(self, n_inputs):
+        super().__init__()
+        self.linear = nn.Linear(n_inputs, 1)
+
+    def forward(self, x):
+        return self.linear(x)
+
+    def train(self, X, y, n_epochs=10, batch_size=100):
+
+        loss = nn.MSELoss()
+        optimizer = optim.Adam(self.parameters(), lr = 0.01)
+        for epoch in range(n_epochs):
+
+            permutation = torch.randperm(X.size()[0])
+
+            for i in range(0, X.size()[0], batch_size):
+                indicies = permutation[i:i+batch_size]
+                X_batch, y_batch = X[indicies], y[indicies]
+                self.zero_grad()
+                output = self(X_batch)
+                loss = F.mse_loss(output, y_batch)
+                loss.backward()
+                optimizer.step()
+
+#Creates feature matrix from state variable
+def featureMatrix(state):
+    state = state.view(state.shape[0], 1)
+    X = torch.cat((state, torch.pow(state, 2), torch.pow(state, 3), torch.pow(state, 4)), dim = 1)
+    return X
+
+#Simulates Black-Scholes paths and calculates exercise value at each t
+def genPaths(S, K, sigma, r, dts, dW):
+    dt = dts.shape[1]
+    r = r * np.arange(1/dt, 1+1/dt, 1/dt)
+    St = S * torch.cumprod(torch.exp((r-sigma**2/2)*dts + sigma*torch.sqrt(dts)*dW), axis=1)
+    Et = torch.exp(-r*1)*torch.maximum(St-K, torch.tensor(0))
+    return St, Et
+
+#Trains Linear models for each timepoint t used to estimate the continuation values.
+#Starts at time T, and iterates backwards through time.
+#Returns model parameters in list
+def LSM_train(St, Et):
+    modelw = [] #list for weights
+    modelb = [] #list for biases
+    n_excerises = St.shape[1]
+
+    St = St.float()
+    Et = Et.float()
+
+    for i in range(n_excerises-1)[::-1]:
+        y = Et[:, i+1:i+2]
+        X = St[:, i]
+        X = featureMatrix(X)
+        tempModel = LinearModel(4)
+        tempModel.train(X[Et[:,i]>0], y[Et[:,i]>0]) #trains on paths ITM
+        continuationValue = tempModel(X) 
+        Et[:, i:i+1] = torch.maximum(Et[:, i:i+1], continuationValue).detach()
+
+        modelw.insert(0, tempModel.linear.weight)
+        modelb.insert(0, tempModel.linear.bias)
+        del continuationValue
+    
+    return modelw, modelb
+
+#Longstaff-Schwartz algorithm using estimated models from LSM_train
+def LSM(S, K, sigma, r, dts, dW, w, b):
+
+    St, Et = genPaths(S, K, sigma, r, dts, dW)
+
+    n_excerises = St.shape[1]
+    continuationValues = []
+    exercises = []
+    previous_exercises = 0
+    npv = 0
+
+    for i in range(n_excerises-1):
+        X = featureMatrix(St[:, i]).double()
+        contValue = torch.add(torch.matmul(X.double(), w[i].T.double()), b[i].double())
+        continuationValues.append(contValue)
+        inMoney = torch.greater(Et[:,i], 0.).float()
+        exercise = torch.greater(Et[:,i], contValue[:,0]).float() * inMoney 
+        exercises.append(exercise)
+        exercise = exercise * (1-previous_exercises)
+        previous_exercises += exercise
+        npv += exercise*Et[:,i]
+    
+    #Last exercise date
+    inMoney = torch.greater(Et[:,-1], 0.).float()
+    exercise =  inMoney * (1-previous_exercises)
+    npv += exercise*Et[:,-1]
+    npv = torch.mean(npv)
+
+    return npv
