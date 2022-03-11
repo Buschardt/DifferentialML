abae19a62f2e85ac7290da9e06c33caf4fdbd404
Magnus <magnumtar@gmail.com>
2022-03-12 00:41:52 +0100

    added antithetic

--- MonteCarlo/EulerSchemeFromProcessModel.py
+++ MonteCarlo/EulerSchemeFromProcessModel.py
@@ -18,34 +18,39 @@ class EulerSchemeFromProcessModel:
         self.product = product
         self.checkCompatability()
 
-    def calculateProcess(self,initialState,pathNumber):
+    def calculateProcess(self,initialState,pathNumber,anti=False):
         self.initialState = initialState
         if self.model.stationary:
             drift = self.model.getDrift(0,0)
             factorLoadings = self.model.getFactorLoadings(0,0)
-            increments = self.stochasticDriver.getIncrement(pathNumber)
+            increments = self.stochasticDriver.getIncrement(pathNumber)*-1 if anti else self.stochasticDriver.getIncrement(pathNumber)
             return torch.exp(torch.log(self.initialState) + torch.cumsum(drift * self.timeDiscretization.deltaT + factorLoadings * increments, axis=0))
         else:
             raise NotImplementedError()
 
-    def pathPayoff(self, initialState, pathNumber):
+    def pathPayoff(self, initialState, pathNumber,anti=False):
         S = self.calculateProcess(initialState, pathNumber)
         V = self.product.payoff(S,
                                 self.model.riskFreeRate,
                                 self.timeDiscretization.deltaT*self.timeDiscretization.numberOfSteps)
-
-        return V
+        if anti:
+            Sa = self.calculateProcess(initialState, pathNumber,anti=True)
+            Va = self.product.payoff(Sa,
+                                    self.model.riskFreeRate,
+                                    self.timeDiscretization.deltaT*self.timeDiscretization.numberOfSteps)
+            return (S+Sa)/2,(V+Va)/2
+        return S,V
     
-    def calculateDerivs(self, initialState, pathNumber,second = False):
+    def calculateDerivs(self, initialState, pathNumber,second = False,anti = False):
         initialState = torch.tensor(initialState).requires_grad_()
         self.model.setAutoGradParams()
         self.model.setDrift()
-        f = self.pathPayoff(initialState, pathNumber)
+        s,f = self.pathPayoff(initialState, pathNumber,anti)
         grad = torch.autograd.grad(f, [initialState]+self.model.getDerivParameters(), allow_unused=True, create_graph=second)
         if second:
             secgrads = torch.autograd.grad(grad[0],[initialState])
-            return grad, secgrads
-        return grad
+            return s,f,grad, secgrads
+        return s,f,grad
     
     def checkCompatability(self):
         if self.model.getNumberOfFactors() != self.stochasticDriver.numberOfFactors:
